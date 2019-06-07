a4f6c8a815837e710057512e9a18e07dd45dbc6b
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-06-06 20:57:33 -0700

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
+#Helper class specifically designed to control logic in our views. This Helpers
+#class will have two class methods:
+#current_user and is_logged_in?.
+
+#These two methods will only ever be called in views, particularly account.erb,
+#in order to add double protection to this view so that only the current user,
+#when they are logged in, can see their bank account balance.
+
+#It's important to note that helper methods can be used for a lot more than just
+#tracking whether a user is logged in and who the current user is. Helpers are
+#methods that make it cleaner to add logic to our views.
+
 class Helpers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
+# models folder notes:
+# contains one file because we only have one model in this app: User
+# validates some of the attributes of our user by writing code(validates_presence_of)
+# that makes sure no one can sign up without inputting their name, email, and password.
+#folder usually has edit.erb, index.erb, new.erb, show.erb
 
-
+class User < ActiveRecord::Base
+  validates_presence_of :name, :password, :balance
+end
